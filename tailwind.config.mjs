9dc3cd0765ca8012a2969f98e870a7e521c3580a
Paul Guachichullca <paulguachichullca023@gmail.com>
2024-06-26 16:43:34 -0500

    Cambio de paleta de colores e incorporacion de fuente cinzel

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,19 +5,25 @@ export default {
 		extend: {
 			colors: {
 				'chill': {
-					'50': '#f2f9f9',
-					'100': '#ddeff0',
-					'200': '#bfe0e2',
-					'300': '#92cace',
-					'400': '#5faab1',
-					'500': '#438e96',
-					'600': '#3b757f',
-					'700': '#356169',
-					'800': '#325158',
-					'900': '#2d464c',
-					'950': '#1a2c32',
+					'50': '#ecfeff',
+					'100': '#cffafe',
+					'200': '#a5f3fc',
+					'300': '#67e8f9',
+					'400': '#22d3ee',
+					'500': '#06b6d4',
+					'600': '#0891b2',
+					'700': '#0e7490',
+					'800': '#155e75',
+					'900': '#164e63',
+					'950': '#083344',
 				},
-
+			},
+			backgroundImage: {
+				'chillinear': "radial-gradient(circle, #06b6d4 10%, #083344 100%)",
+				'textlinear': "linear-gradient(180deg, rgba(240,218,43,1) 15%, rgba(219,36,0,1) 70%, rgba(105,17,0,1) 99%)",
+			},
+			fontFamily: {
+				'cinzel': 'Cinzel Decorative, system-ui'
 			}
 		},
 	},
